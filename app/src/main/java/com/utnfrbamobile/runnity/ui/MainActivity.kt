da621016d8c4e13df286b4bf67777554009af3b7
Fernando Iglesias <feriglesias@frba.utn.edu.ar>
2021-11-03 18:09:01 -0300

    Agrego pantalla de login y navegacion entre pantallas

--- app/src/main/java/com/utnfrbamobile/runnity/ui/MainActivity.kt
+++ app/src/main/java/com/utnfrbamobile/runnity/ui/MainActivity.kt
@@ -2,11 +2,41 @@ package com.utnfrbamobile.runnity.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.utnfrbamobile.runnity.R
+import com.utnfrbamobile.runnity.auth.LoginFragment
+import com.utnfrbamobile.runnity.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LoginFragment.OnFragmentInteractionListener {
+    private lateinit var loginFragment: Fragment
+    private lateinit var mapFragment: Fragment
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            loginFragment = LoginFragment()
+
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, loginFragment)
+                .commitNow()
+        }
+    }
+
+    override fun onLogin(username: String, password: String) {
+        mapFragment = MapFragment()
+
+        supportFragmentManager.beginTransaction()
+            .remove(loginFragment)
+            .add(R.id.container, mapFragment)
+            .commitNow()
+    }
+
+    override fun onSignUp() {
+
     }
 }
