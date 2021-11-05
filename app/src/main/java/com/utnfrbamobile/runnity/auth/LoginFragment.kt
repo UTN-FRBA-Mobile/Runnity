0d818a2fc356b4f0958ec8b54a8c9c0e8f951def
Fernando Iglesias <feriglesias@frba.utn.edu.ar>
2021-11-04 23:06:42 -0300

    Cambios minimos en el login

--- app/src/main/java/com/utnfrbamobile/runnity/auth/LoginFragment.kt
+++ app/src/main/java/com/utnfrbamobile/runnity/auth/LoginFragment.kt
@@ -30,9 +30,7 @@ class LoginFragment : Fragment() {
             when{
                 username.isEmpty() -> Toast.makeText(activity, "Ingrese su nombre de usuario", Toast.LENGTH_SHORT).show()
                 password.isEmpty() -> Toast.makeText(activity, "Ingrese su password", Toast.LENGTH_SHORT).show()
-                else -> {
-                    findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToMapFragment())
-                }
+                else -> findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToMapFragment())
             }
         }
 
