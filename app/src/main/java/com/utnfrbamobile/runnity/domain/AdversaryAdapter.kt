d80bc77a1ea27427362cd1363ac58adc5d82a61c
Fernando Iglesias <feriglesias@frba.utn.edu.ar>
2021-11-29 18:17:12 -0300

    Mostrar oponente seleccionado

--- app/src/main/java/com/utnfrbamobile/runnity/domain/AdversaryAdapter.kt
+++ app/src/main/java/com/utnfrbamobile/runnity/domain/AdversaryAdapter.kt
@@ -1,5 +1,6 @@
 package com.utnfrbamobile.runnity.domain
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,9 @@ import androidx.recyclerview.widget.RecyclerView
 import com.utnfrbamobile.runnity.R
 
 class AdversaryAdapter(private val users: List<User>, private val listener: OnAdversarySelectedListener): RecyclerView.Adapter<AdversaryAdapter.ViewHolder>() {
+
+    lateinit var selectedLabel: TextView
+    var isSelected = false
 
     override fun getItemCount() = users.size
 
@@ -19,6 +23,16 @@ class AdversaryAdapter(private val users: List<User>, private val listener: OnAd
         val adversary = users[position]
         holder.itemView.findViewById<TextView>(R.id.adversary_name_label).text = adversary.name
         holder.itemView.setOnClickListener {
+            if(isSelected){
+                selectedLabel.setBackgroundColor(Color.parseColor("#0000FFFF"))
+                selectedLabel.setTextColor(Color.parseColor("black"))
+            }
+
+            isSelected = true
+            selectedLabel = holder.itemView.findViewById<TextView>(R.id.adversary_name_label)
+
+            selectedLabel.setBackgroundColor(Color.parseColor("#FF6200EE"))
+            selectedLabel.setTextColor(Color.parseColor("#FFFFFFFF"))
             listener.onAdversarySelected(adversary.email, adversary.name)
         }
     }
